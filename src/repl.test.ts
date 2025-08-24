ea914bb01d57f5abb913d7ce57ee6247efe3f5ce
sparkingsquirrel@gmail.com <sparkingsquirrel@gmail.com>
2025-08-24 18:46:11 -0500

    add map fucntion

--- src/repl.test.ts
+++ src/repl.test.ts
@@ -1,32 +1,28 @@
-import { cleanInput } from "./repl";
+import { cleanInput } from "./repl.js";
 import { describe, expect, test } from "vitest";
 
 describe.each([
+  {
+    input: "  ",
+    expected: [],
+  },
+  {
+    input: "  hello  ",
+    expected: ["hello"],
+  },
   {
     input: "  hello  world  ",
     expected: ["hello", "world"],
   },
   {
-    input: "  heLLo  world!  ",
-    expected: ["hello", "world!"],
+    input: "  HellO  World  ",
+    expected: ["hello", "world"],
   },
-  {
-    input: "", //not actually sure what the desired response here is
-    expected: [""],
-  },
-  {
-    input: "three word test",
-    expected: ["three", "word", "test"],
-  },
-  
 ])("cleanInput($input)", ({ input, expected }) => {
   test(`Expected: ${expected}`, () => {
     const actual = cleanInput(input);
-    // The `expect` and `toHaveLength` functions are from vitest
-    // they will fail the test if the condition is not met
     expect(actual).toHaveLength(expected.length);
     for (const i in expected) {
-      // likewise, the `toBe` function will fail the test if the values are not equal
       expect(actual[i]).toBe(expected[i]);
     }
   });
